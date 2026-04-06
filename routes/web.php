d0963acc7a5eab03a3d940c4a675e8cfe000d687
boot88 <116805971+boot88@users.noreply.github.com>
2026-04-06 16:55:17 +0700

    Fix alarm toggle persistence on main list

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\AlarmController;
 Route::redirect('/', '/alarms'); // ✅ главная ведёт на список будильников
 
 Route::get('/alarms/due', [AlarmController::class, 'due'])->name('alarms.due');
+Route::patch('/alarms/{alarm}/enabled', [AlarmController::class, 'toggleEnabled'])->name('alarms.toggle-enabled');
 
 
 
